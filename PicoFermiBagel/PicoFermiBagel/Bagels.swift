eb750500f3440edb4744cea8f53c3797fa11ac05
Byron Jenkins <codeherk@gmail.com>
2019-02-14 14:20:28 -0500

    template for Lab 1

---
+++ PicoFermiBagel/PicoFermiBagel/Bagels.swift
@@ -0,0 +1,53 @@
+// Byron Jenkins
+// CIS 3513 Lab 1 - Pico, Fermi, Bagel
+//
+// Bagels.swift
+//
+// Lab 1 - Pico, Fermi, Bagel
+//  1. The computer will generate a "secret" three digit number at random.
+//  2. The first number will not be 0, and all the digits will be different.
+//  3. The user tries to guess the number.
+//      - If the user guesses correctly, then the game is over.
+//      - If not, the computer gives a hint and the player tries again.
+
+
+//  Bagels class should have 4 methods
+//      - playGame()
+//      - generateSecretNumber()
+//      - isGuessCorrect(guess: String) -> Bool
+//      - printHint(guess: String)
+
+// Your Bagels class will have only one public method, playGame().
+// This method will call three other methods to:
+//      1. Generate the secret number
+//      2. Determine whether the current guess is a winner
+//      3. Evaluate the current guess and print hints
+
+import Foundation
+
+class Bagels {
+    
+    // DO NOT MODIFY THE METHOD DECLARATIONS IN ANY WAY
+    
+    // the ONLY public method
+    public func playGame(){
+        // read user input
+        
+        
+    }
+    
+    private func generateSecretNumber(){
+        // random number generation
+    
+    }
+    
+    private func isGuessCorrect(guess: String) -> Bool{
+        
+        
+    }
+    
+    private func printHint(guess: String){
+        
+    }
+    
+}
---
+++ PicoFermiBagel/PicoFermiBagel/main.swift
@@ -0,0 +1,18 @@
+// Byron Jenkins
+// CIS 3513 Lab 1 - Pico, Fermi, Bagel
+//
+// main.Swift
+//
+// Your main class will create a Bagels object and call the method that plays the game.
+// After each game, give the user the option of playing another.
+// After the last game, thank the user. That’s all, nothing else.
+
+
+import Foundation
+
+// create Bagels object
+let bagels =  Bagels()
+
+// call the method that plays the game
+bagels.playGame()
+
